9e49b203826a4d6f3ea83445c8e2718cdef09950
Raleigh Dale <dalearaleigh@gmail.com>
2019-04-22 17:35:58 +0000

    video uploading

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,9 +1,11 @@
 CarrierWave.configure do |config|
-  config.fog_provider = 'fog/aws'                        # required
-  config.fog_credentials = {
-    provider:              'AWS',                        # required
-    aws_access_key_id:     ENV["AWS_ACCESS_KEY"],        # required
-    aws_secret_access_key: ENV["AWS_SECRET_KEY"],        # required
+  config.storage    = :aws
+  config.aws_bucket = ENV["flixter-rad-user"]
+  config.aws_acl    = "public-read"
+
+  config.aws_credentials = {
+      access_key_id:     ENV["AKIAYYLZFQC7QRSSA76Q"],
+      secret_access_key: ENV["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"],
+      region:            ENV["AWS_REGION"]
   }
-  config.fog_directory  = ENV["AWS_BUCKET"]              # required
-end
+end 
