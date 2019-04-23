24b5c5a73de4b6635bce58a83659d3321a36dcf6
Raleigh Dale <dalearaleigh@gmail.com>
2019-04-23 19:13:34 +0000

    updating carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,8 +4,8 @@ CarrierWave.configure do |config|
   config.aws_acl    = "public-read"
 
   config.aws_credentials = {
-      access_key_id:     ENV["AKIAYYLZFQC7QRSSA76Q"],
-      secret_access_key: ENV["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"],
+      access_key_id:     ENV["AWS_ACCESS_KEY"],
+      secret_access_key: ENV["AWS_SECRET_KEY"],
       region:            ENV["AWS_REGION"]
   }
 end 
