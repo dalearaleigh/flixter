0f413d26a8255690aec96293de239e05c63a55f6
Raleigh Dale <dalearaleigh@gmail.com>
2019-05-01 21:58:43 +0000

    lesosn drag and drop

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,12 @@ Rails.application.routes.draw do
   end
   resources :lessons, only: [:show]
   namespace :instructor do
+    resources :lessons, only: [:update]
     resources :sections, only: [] do
-    resources :lessons, only: [:new, :create]
+      resources :lessons, only: [:new, :create]
     end
     resources :courses, only: [:new, :create, :show] do
       resources :sections, only: [:new, :create]
     end
   end
-end 
+end
